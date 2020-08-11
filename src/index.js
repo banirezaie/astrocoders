730f5893992aaec1a010629d4f4776f789f29a61
BANI GIT <bani.rezaie@gmail.com>
2020-08-11 19:19:21 +0100

    deploying

--- src/index.js
+++ src/index.js
@@ -21,31 +21,31 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-client.connect(function () {
-  const db = client.db("attandence");
-  const collection = db.collection("students");
+// client.connect(function () {
+//   const db = client.db("attandence");
+//   const collection = db.collection("students");
 
-  app.get("/", (req, res) => {
-    res.send("<h2>You can search the students now!</h2>");
-  });
-
-  // create a  /attendance page which includes a form. Our form allow students to enter: Name, Email Address, Date
-  app.post("/attendance", (req, res) => {
-    const addAttandence = {
-      name: req.body.name,
-      email: req.body.email,
-      date: new Date(req.body.date),
-    };
-
-    collection.insertOne(addAttandence, function (error, result) {
-      if (error) {
-        res.status(500).send(error);
-      }
-      res.status(200).send(result.ops[0]);
-
-      client.close;
-    });
-  });
+app.get("/", (req, res) => {
+  res.send("<h2>You can search the students now!</h2>");
 });
+
+//   // create a  /attendance page which includes a form. Our form allow students to enter: Name, Email Address, Date
+//   app.post("/attendance", (req, res) => {
+//     const addAttandence = {
+//       name: req.body.name,
+//       email: req.body.email,
+//       date: new Date(req.body.date),
+//     };
+
+//     collection.insertOne(addAttandence, function (error, result) {
+//       if (error) {
+//         res.status(500).send(error);
+//       }
+//       res.status(200).send(result.ops[0]);
+
+//       client.close;
+//     });
+//   });
+// });
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
