b2d7c5d663f403721ca01ef8ff221e077ff64521
mahmutsel <mahmutsel29@gmail.com>
2020-08-12 10:48:00 +0100

    frontend and backend connected. It is adding the info to database.

--- frontend/src/students/ClassType.js
+++ frontend/src/students/ClassType.js
@@ -1,41 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import "../App.css";
 
-const ClassType = () => {
+const ClassType = ({name,email}) => {
+  const [myClass, setMyClass] = useState("");
+  const [type, setType] = useState("");
+  
+  function handleSubmit() {
+    const body = JSON.stringify({
+      myClass,
+      type,
+      name,
+      email
+    });
+
+    fetch(`http://localhost:9000/attendance`, {
+      method: "POST",
+      referrer: "",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body,
+    })
+      .then((res) => res.json())
+      .then();
+  }
+
   return (
     <div className="App-header">
       <div className="col-6  mx-auto">
         <div className="text-center pb-5">
           <h1>Attend class</h1>
         </div>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="form-group">
             <label htmlFor="class">Class</label>
-            <select className="form-control" id="class">
-              <option>1</option>
-              <option>2</option>
-              <option>3</option>
-              <option>4</option>
-              <option>5</option>
+            <select
+              className="form-control"
+              id="class"
+              onChange={(e) => setMyClass(e.target.value)}
+            >
             </select>
           </div>
           <div className="form-group">
             <label htmlFor="type">Type</label>
-            <select className="form-control" id="type">
-              <option selected>class</option>
+            <select
+              className="form-control"
+              id="type"
+              onChange={(e) => setType(e.target.value)}
+            >
+              <option>...</option>
+              <option>class</option>
               <option>Homework club</option>
+              <option>Other</option>
             </select>
           </div>
-          <div className="form-group">
-            <label htmlFor="date">Date</label>
-            <select className="form-control" id="date">
-              <option>1</option>
-              <option>2</option>
-              <option>3</option>
-              <option>4</option>
-              <option>5</option>
-            </select>
-          </div>
+
           <div className="text-center">
             <button type="submit" className="btn btn-success">
               Submit
