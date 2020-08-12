0ec69d5352f11f5565033026343c78cc8510c6d1
mahmutsel <mahmutsel29@gmail.com>
2020-08-12 10:48:18 +0100

    name and email stoed in client states

--- frontend/src/students/Students.js
+++ frontend/src/students/Students.js
@@ -1,8 +1,11 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import ClassType from "./ClassType";
+import { NavLink } from "react-router-dom";
 import "../App.css";
 
 const Students = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
   return (
     <div className="App-header">
       <div className="col-6  mx-auto">
@@ -12,7 +15,13 @@ const Students = () => {
         <form>
           <div className="form-group">
             <label htmlFor="name">Name</label>
-            <input type="text" className="form-control" id="name" />
+            <input
+              type="text"
+              className="form-control"
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
           </div>
           <div className="form-group">
             <label htmlFor="email">Email address</label>
@@ -21,20 +30,25 @@ const Students = () => {
               className="form-control"
               id="email"
               aria-describedby="emailHelp"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
             />
             <small id="emailHelp" className="form-text text-muted">
               We'll never share your email with anyone else.
             </small>
           </div>
+
           <div className="text-center">
-            <Link
+            <NavLink
               type="submit"
               className="btn btn-primary"
               to="/students/classtype"
             >
               Next
-            </Link>
+            </NavLink>
           </div>
+          <ClassType name={name} email={email} />
         </form>
       </div>
     </div>
