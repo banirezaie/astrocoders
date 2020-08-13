dd92134ad980c14110c69e7d159765cb6fd7ced1
BANI GIT <bani.rezaie@gmail.com>
2020-08-13 14:23:36 +0100

    navbar

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -16,7 +16,17 @@ const Navbar = () => {
       >
         <span className="navbar-toggler-icon"></span>
       </button>
-
+      <form className="form-inline my-2 my-lg-0">
+        <input
+          className="form-control mr-sm-2"
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+        />
+        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
+          Search
+        </button>
+      </form>
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item active">
@@ -65,20 +75,6 @@ const Navbar = () => {
             </a>
           </li>
         </ul>
-        <form className="form-inline my-2 my-lg-0">
-          <input
-            className="form-control mr-sm-2"
-            type="search"
-            placeholder="Search"
-            aria-label="Search"
-          />
-          <button
-            className="btn btn-outline-success my-2 my-sm-0"
-            type="submit"
-          >
-            Search
-          </button>
-        </form>
       </div>
     </nav>
   );
