537cc70e6f369bbc5eade4cbf3596df23c43528e
melanieet <melanie@melanies-mbp.home>
2020-08-13 16:05:11 +0100

    Removed unneccessary search bar

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -19,17 +19,7 @@ const Navbar = () => {
       >
         <span className="navbar-toggler-icon"></span>
       </button>
-      <form className="form-inline my-2 my-lg-0">
-        <input
-          className="form-control mr-sm-2"
-          type="search"
-          placeholder="Search"
-          aria-label="Search"
-        />
-        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
-          Search
-        </button>
-      </form>
+
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item active">
