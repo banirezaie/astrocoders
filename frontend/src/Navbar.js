89f617b9003be6512bef8d6576905e3082b9010e
BANI GIT <bani.rezaie@gmail.com>
2020-08-13 11:30:56 +0100

    navbar

---
+++ frontend/src/Navbar.js
@@ -0,0 +1,87 @@
+import React from "react";
+import "./App.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <img src="logo.png" className="logo ml-4" />
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#somethingnavbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <a className="nav-link" href="https://codeyourfuture.io/about/">
+              About
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="something">
+              Link
+            </a>
+          </li>
+          <li className="nav-item dropdown">
+            <a
+              className="nav-link dropdown-toggle"
+              href="something"
+              id="navbarDropdown"
+              role="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Dropdown
+            </a>
+            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a className="dropdown-item" href="something">
+                Action
+              </a>
+              <a className="dropdown-item" href="something">
+                Another action
+              </a>
+              <div className="dropdown-divider"></div>
+              <a className="dropdown-item" href="something">
+                Something else here
+              </a>
+            </div>
+          </li>
+          <li className="nav-item">
+            <a
+              className="nav-link disabled"
+              href="something"
+              tabIndex="-1"
+              aria-disabled="true"
+            >
+              Disabled
+            </a>
+          </li>
+        </ul>
+        <form className="form-inline my-2 my-lg-0">
+          <input
+            className="form-control mr-sm-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button
+            className="btn btn-outline-success my-2 my-sm-0"
+            type="submit"
+          >
+            Search
+          </button>
+        </form>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- frontend/src/mentors/Mentors.js
+++ frontend/src/mentors/Mentors.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import Navbar from "../Navbar";
 import "../App.css";
 
 const Mentors = () => {
@@ -9,48 +10,51 @@ const Mentors = () => {
       .then((data) => setStudents(data));
   }, []);
   return (
-    <div className="App-header">
-      <table className="table table-striped container bg-white table-hover">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">Name</th>
-            <th scope="col">Email</th>
-            <th scope="col">Class</th>
-            <th scope="col">Type</th>
-            <th scope="col">Date</th>
-            <th scope="col">Time</th>
-          </tr>
-        </thead>
-        {students ? (
-          <tbody>
+    <div>
+      <Navbar />
+      <div className="App-header">
+        <table className="table table-striped container bg-white table-hover">
+          <thead>
             <tr>
-              <th scope="row">1</th>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
+              <th scope="col">#</th>
+              <th scope="col">Name</th>
+              <th scope="col">Email</th>
+              <th scope="col">Class</th>
+              <th scope="col">Type</th>
+              <th scope="col">Date</th>
+              <th scope="col">Time</th>
             </tr>
-            <tr>
-              <th scope="row">2</th>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-            </tr>
-            <tr>
-              <th scope="row">3</th>
-              <td>Larry</td>
-              <td>the Bird</td>
-              <td>@twitter</td>
-            </tr>
-          </tbody>
-        ) : (
-          <tbody>
-            <tr>
-              <th>No one attended yet</th>
-            </tr>
-          </tbody>
-        )}
-      </table>
+          </thead>
+          {students ? (
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>@twitter</td>
+              </tr>
+            </tbody>
+          ) : (
+            <tbody>
+              <tr>
+                <th>No one attended yet</th>
+              </tr>
+            </tbody>
+          )}
+        </table>
+      </div>
     </div>
   );
 };
