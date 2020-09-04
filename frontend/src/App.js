61404fe34aee5053da5fd6518256a63a7dc5c663
mahmutsel <mahmutsel29@gmail.com>
2020-09-04 17:50:44 +0100

    authentication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, createContext, useEffect } from "react";
+import { auth } from "./firebase";
 import Home from "./Home";
 import Students from "./students/Students";
 import { Route, Switch } from "react-router-dom";
@@ -12,25 +13,36 @@ import AttendeeList from "./attendees/AttendeeList";
 import LocationUpdate from "./admin/LocationUpdate";
 import AddLocation from "./admin/AddGroup";
 import LoginPage from "./LoginPage";
+import SignIn from "./authentication/SignIn";
+import SignUp from "./authentication/SignUp";
+import PasswordReset from "./authentication/PasswordReset";
+import UserProvider from "./providers/UserProvider";
+// const UserContext = createContext({ user: null });
 
 function App() {
+
+  // useEffect(() => {
+  //   auth.onAuthStateChanged(setUser);
+  // });
   return (
-    <Switch>
-      <Route path="/" component={Home} exact />
-      <Route path="/students" component={Students} exact />
-      <Route path="/locations" component={LocationUpdate} />
-
-      <Route path="/attendees" component={AttendeeList} />
-      <Route path="/add-location" component={AddLocation} />
-
-      <Route exact path="/groups" component={Groups} />
-      <Route path="/groups/:id/details" component={GroupDetail} />
-
-      <Route path="/admin" component={Admin} />
-      <Route path="/CreateCode" component={CreateClassCode} />
-      <Route path="/class-code" component={ClassCodes} />
-      <Route path="/login" component={LoginPage} />
-    </Switch>
+    <UserProvider>
+      <Switch>
+        <Route path="/sign-in" component={SignIn} />
+        <Route path="/sign-up" component={SignUp} />
+        <Route path="/password-reset" component={PasswordReset} />
+        <Route path="/" component={Home} exact />
+        <Route path="/students" component={Students} exact />
+        <Route path="/locations" component={LocationUpdate} />
+        <Route path="/attendees" component={AttendeeList} />
+        <Route path="/add-location" component={AddLocation} />
+        <Route exact path="/groups" component={Groups} />
+        <Route path="/groups/:id/details" component={GroupDetail} />
+        <Route path="/admin" component={Admin} />
+        <Route path="/CreateCode" component={CreateClassCode} />
+        <Route path="/class-code" component={ClassCodes} />
+        <Route path="/login" component={LoginPage} />
+      </Switch>
+    </UserProvider>
   );
 }
 
