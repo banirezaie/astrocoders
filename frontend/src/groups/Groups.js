1566f4c375ea2d44d103311da989bea4ee830698
melanieet <melanie@melanies-mbp.home>
2020-09-19 21:35:41 +0100

    Update Groups.js

--- frontend/src/groups/Groups.js
+++ frontend/src/groups/Groups.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from "react";
 import "../App.css";
 import { Link } from "react-router-dom";
+import MentorsNavbar from "../navbar/MentorsNavbar";
 
 const Mentors = () => {
   const [students, setStudents] = useState("");
-      const apiBaseUrl =
-        process.env.NODE_ENV === "production"
-          ? process.env.REACT_APP_PROD_API_URL
-          : process.env.REACT_APP_LOCAL_API_URL;
+  const apiBaseUrl =
+    process.env.NODE_ENV === "production"
+      ? process.env.REACT_APP_PROD_API_URL
+      : process.env.REACT_APP_LOCAL_API_URL;
 
   useEffect(() => {
     fetch(`${apiBaseUrl}/admins`)
@@ -21,7 +22,12 @@ const Mentors = () => {
 
   return (
     <div>
-      <div className="header">
+      <MentorsNavbar
+        background="#aaa"
+        hoverBackground="#ddd"
+        linkColor="#eee"
+      />
+      <div style={{ paddingTop: "75px" }} className="header">
         <h2>Group List</h2>
       </div>
       <div className="table">
