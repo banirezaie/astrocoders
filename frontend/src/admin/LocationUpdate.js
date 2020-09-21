4a3c91a662112fe3ceff7498b5391f368aad274e
BANI GIT <bani.rezaie@gmail.com>
2020-09-21 13:39:25 +0100

    location

--- frontend/src/admin/LocationUpdate.js
+++ frontend/src/admin/LocationUpdate.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect,useCallback } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import "../App.css";
-// import { Link } from "react-router-dom";
 import AddGroup from "../admin/AddGroup";
 import ViewGroups from "./ViewGroups";
 import AddLocation from "./AddLocation";
 import DeleteLocation from "./DeleteLocation";
+import AdminNavbar from "../navbar/AdminNavbar";
 
 const LocationUpdate = ({ props }) => {
   const [location, setLocation] = useState("");
@@ -31,89 +31,86 @@ const LocationUpdate = ({ props }) => {
   const handleOnDeleteLocation = () => {
     loadLocation();
   };
-  // const handleOnAddLocation = () => {
-  //   loadLocation();
-  // };
-
 
   return (
-    <div className="wrapper">
-      <div className="list-jumbotron">
-        <div className="col-sm-12 col-md-4">
-          <h2>Location List</h2>
+    <div className="same-background">
+      <AdminNavbar background="#aaa" hoverBackground="#ddd" linkColor="#eee" />
+      <div className="wrapper">
+        <div className="margin-top row justify-content-between width">
+          <div className="col-4">
+            <h1 className="text-white">Location List</h1>
+          </div>
+          <div className="col-4">
+            <AddLocation />
+          </div>
         </div>
-
-        <hr className="my-4"></hr>
-        <div className="col-sm-12 col-md-4">
-          <AddLocation />
-        </div>
-      </div>
-      {location.map((data, i) => {
-        return (
-          <div className="container">
-            <div className="row">
-              <div className="col-sm-4 col-md-4">
-                <div className="card">
-                  <div className="card-body">
-                    <h4 className="card-city">{data.name}</h4>
-                    <DeleteLocation
-                      props={{
-                        id: data._id,
-                        onDeleteLocation: handleOnDeleteLocation,
-                      }}
-                    />
-                  </div>
-                </div>
-              </div>
-              <div className="col-sm-4 col-md-4">
-                <div className="card">
-                  <div className="card-body">
-                    <h5 className="card-title">Group Names</h5>
-
-                    <ul className="list-group ">
-                      <ViewGroups
-                        id={data._id}
-                        groups={data.groups}
-                        setGroups={(group) =>
-                          setLocation(
-                            location.map((oldLocation) =>
-                              oldLocation === data
-                                ? { ...data, groups: group }
-                                : oldLocation
-                            )
-                          )
-                        }
+        {location.map((data, i) => {
+          return (
+            <div className="container">
+              <div className="row">
+                <div className="col-sm-4 col-md-4">
+                  <div className="card">
+                    <div className="card-body">
+                      <h4 className="card-city">{data.name}</h4>
+                      <DeleteLocation
+                        props={{
+                          id: data._id,
+                          onDeleteLocation: handleOnDeleteLocation,
+                        }}
                       />
-                    </ul>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="col-sm-4 col-md-4">
-                <div className="card">
-                  <div className="card-body">
-                    <AddGroup
-                      props={{
-                        id: data._id,
-                        onAddGroup: (group) =>
-                          setLocation(
-                            location.map((oldLocation) =>
-                              oldLocation === data
-                                ? {
-                                    ...data,
-                                    groups: data.groups.concat([group]),
-                                  }
-                                : oldLocation
+                <div className="col-sm-4 col-md-4">
+                  <div className="card">
+                    <div className="card-body">
+                      <h5 className="card-title">Group Names</h5>
+
+                      <ul className="list-group ">
+                        <ViewGroups
+                          id={data._id}
+                          groups={data.groups}
+                          setGroups={(group) =>
+                            setLocation(
+                              location.map((oldLocation) =>
+                                oldLocation === data
+                                  ? { ...data, groups: group }
+                                  : oldLocation
+                              )
                             )
-                          ),
-                      }}
-                    />
+                          }
+                        />
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-sm-4 col-md-4">
+                  <div className="card">
+                    <div className="card-body">
+                      <AddGroup
+                        props={{
+                          id: data._id,
+                          onAddGroup: (group) =>
+                            setLocation(
+                              location.map((oldLocation) =>
+                                oldLocation === data
+                                  ? {
+                                      ...data,
+                                      groups: data.groups.concat([group]),
+                                    }
+                                  : oldLocation
+                              )
+                            ),
+                        }}
+                      />
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
   );
 };
